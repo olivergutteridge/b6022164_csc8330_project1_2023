ca02e5af2da7659831189e70683e4213d106fa8b
olivergutteridge <olivergutteridge@gmail.com>
2023-02-06 18:43:33 +0000

    formatting README.md

--- plotting_graphs/cli.py
+++ plotting_graphs/cli.py
@@ -6,7 +6,7 @@ def cli():
     parser.add_argument("--no-repeat", help="suppress repeat", action="store_true") 
     parser.add_argument("--interval", help="set interval (default 20)", default=20, action="store", type=int, choices=range(1, 31))
     parser.add_argument("--frequency", help="set frequency (default 2)", default=2, action="store", type=int)
-    parser.add_argument("--amplitude", help="set amplitude (default 1)", default=1, action="store", type=float)
+    parser.add_argument("--amplitude", help="set amplitude (default 1)", default=1, action="store", type=float, choices=range(0.0,1.01))
     parser.add_argument("--colour", help="set the colour of the wave (default blue)", default="blue", action="store", type=str)
     args = parser.parse_args()
 
