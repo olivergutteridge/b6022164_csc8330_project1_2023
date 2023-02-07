83287f4b4a60d6ef68bad784bc8d0cbe75edb43b
olivergutteridge <olivergutteridge@gmail.com>
2023-02-07 18:18:46 +0000

    edit --speed argument

--- plotting_graphs/cli.py
+++ plotting_graphs/cli.py
@@ -5,7 +5,7 @@ import argparse
 def cli():
     parser = argparse.ArgumentParser(prog="plotting_graphs", description="A simple program that plots a Sine waveform graph")
     parser.add_argument("--no-repeat", help="Suppress repeat", action="store_true") 
-    parser.add_argument("--speed", help="Set wave speed", default=5, action="store", type=int, choices=range(1, 11), metavar="INTERVAL")
+    parser.add_argument("--speed", help="Set wave speed", default=5, action="store", type=int, choices=range(1, 11), metavar="SPEED")
     parser.add_argument("--frequency", help="Set wave frequency", default=2, action="store", type=int, metavar="FREQUENCY")
     parser.add_argument("--amplitude", help="Set wave amplitude", default=1, action="store", type=float, choices=np.arange(0.0,1.01,0.01), metavar="AMPLITUDE")
     parser.add_argument("--colour", help="Set wave colour", default="blue", action="store", type=str, metavar="COLOUR")
