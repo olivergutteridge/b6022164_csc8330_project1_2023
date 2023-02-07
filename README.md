4f9fbaeb50064aab98e93327dce3ad3253a962c3
olivergutteridge <olivergutteridge@gmail.com>
2023-02-07 18:17:42 +0000

    --interval -> --speed

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Install dependicies using the command ```pip3 install -r requirements.txt```.
 | Argument | Description | Default | Example | 
 | -------- | ----------- | ------- | ------- |
 | --no-repeat | Stop animation. | False | ```python3.11 plotting_graphs.py --no-repeat``` |
-| --interval | Alter the animation speed. | 20 | ```python3.11 plotting_graphs.py --no-repeat``` |
+| --speed | Alter the animation speed. | 5 | ```python3.11 plotting_graphs.py --speed 7``` |
 | --frequency | Alter the sine wave frequency. | 2 | ```python3.11 plotting_graphs.py --frequency 50``` |
 | --amplitude | Alter the sine wave amplitude. | 1 | ```python3.11 plotting_graphs.py --amplitude 0.5``` |
 | --colour | Select a colour for the sine wave (matplotlib v3.6.3). | Blue | ```python3.11 plotting_graphs.py --colour green``` |
--- plotting_graphs/line_plot.py
+++ plotting_graphs/line_plot.py
@@ -3,9 +3,9 @@ import matplotlib.animation as animation
 import numpy as np
 
 class LinePlot():
-    def __init__(self, no_repeat=False, interval=20, frequency=2, amplitude=1, colour="blue"):
+    def __init__(self, no_repeat=False, speed=1, frequency=2, amplitude=1, colour="blue"):
         self.no_repeat = no_repeat
-        self.interval = interval
+        self.speed = 100 * (1 - (speed / 10)) + (speed / 2)
         self.frequency = frequency
         self.amplitude = amplitude
         self.colour = colour
@@ -15,13 +15,13 @@ class LinePlot():
         self.line.set_color(self.colour)
         
     def animate(self, i):
-        self.line.set_ydata(np.sin(self.x + i / 50)*self.amplitude)  # update the data.
+        self.line.set_ydata(np.sin(self.x + i / self.speed)*self.amplitude)  # update the data.
         return self.line,
 
     def plot(self):
         if self.no_repeat == False:
             ani = animation.FuncAnimation(
-                self.fig, self.animate, frames=320, interval=self.interval, blit=True, save_count=50)
+                self.fig, self.animate, frames=320, interval=20, blit=True, save_count=50)
         if self.no_repeat == True:
             pass
         plt.show()
