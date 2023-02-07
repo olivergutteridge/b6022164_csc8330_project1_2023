0c53e95b6a5a802877efc1fac24daf89aef3fbae
olivergutteridge <olivergutteridge@gmail.com>
2023-02-07 16:40:36 +0000

    formatting

--- plotting_graphs/line_plot.py
+++ plotting_graphs/line_plot.py
@@ -4,15 +4,18 @@ import numpy as np
 
 class LinePlot():
     def __init__(self, no_repeat=False, interval=20, frequency=2, amplitude=1, colour="blue"):
-        self.fig, self.ax = plt.subplots()
-        self.x = np.arange(0, frequency*np.pi, 0.01)
-        self.line, = self.ax.plot(self.x, (np.sin(self.x))/amplitude)
-        self.line.set_color(colour)
         self.no_repeat = no_repeat
         self.interval = interval
-
+        self.frequency = frequency
+        self.amplitude = amplitude
+        self.colour = colour
+        self.fig, self.ax = plt.subplots()
+        self.x = np.arange(0, self.frequency*np.pi, 0.01)
+        self.line, = self.ax.plot(self.x, np.sin(self.x))
+        self.line.set_color(self.colour)
+        
     def animate(self, i):
-        self.line.set_ydata(np.sin(self.x + i / 50))  # update the data.
+        self.line.set_ydata(np.sin(self.x + i / 50)*self.amplitude)  # update the data.
         return self.line,
 
     def plot(self):
