0d27c0bb190d4cba5b48f6684169ccc80d900118
olivergutteridge <olivergutteridge@gmail.com>
2023-02-08 17:58:33 +0000

    edit arguments

--- plotting_graphs/cli.py
+++ plotting_graphs/cli.py
@@ -3,12 +3,12 @@ import numpy as np
 import argparse
 
 def cli():
-    parser = argparse.ArgumentParser(prog="plotting_graphs", description="A simple program that plots a Sine waveform graph")
-    parser.add_argument("--no-repeat", help="Suppress repeat", action="store_true") 
-    parser.add_argument("--speed", help="Set wave speed", default=5, action="store", type=int, choices=range(1, 11), metavar="SPEED")
-    parser.add_argument("--frequency", help="Set wave frequency", default=2, action="store", type=int, metavar="FREQUENCY")
-    parser.add_argument("--amplitude", help="Set wave amplitude", default=1, action="store", type=float, choices=np.arange(0.0,1.01,0.01), metavar="AMPLITUDE")
-    parser.add_argument("--colour", help="Set wave colour", default="blue", action="store", type=str, metavar="COLOUR")
+    parser = argparse.ArgumentParser(prog="plotting_graphs", description="a simple program that plots a sine waveform graph")
+    parser.add_argument("--no-repeat", help="suppress repeat", action="store_true") 
+    parser.add_argument("--speed", help="set wave speed, int: 1->10", default=5, action="store", type=int, choices=range(1, 11), metavar="SPEED")
+    parser.add_argument("--frequency", help="set wave frequency, int: 1->10", default=2, action="store", type=int, choices=range(1, 11), metavar="FREQUENCY")
+    parser.add_argument("--amplitude", help="set wave amplitude, float: 0.0->1.0", default=1, action="store", type=float, choices=np.arange(0.0, 1.01, 0.01), metavar="AMPLITUDE")
+    parser.add_argument("--colour", help="set wave colour, str: colour from matplotlib package", default="blue", action="store", type=str, metavar="COLOUR")
     args = parser.parse_args()
 
     animation = line_plot.LinePlot(**vars(args))
